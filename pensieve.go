89894435279418280af32dff4fe9e6e3d1f9e9e6
Brian Muller <bamuller@gmail.com>
2018-08-17 21:43:55 -0400

    updated for new pilosa api

--- pensieve.go
+++ pensieve.go
@@ -25,7 +25,7 @@ type KruxBitIterator struct {
 
 type SegmentIDMapper struct {
 	client *pilosa.Client
-	frame  *pilosa.Frame
+	field  *pilosa.Field
 	cache  map[string]uint64
 	rowcnt uint64
 }
@@ -34,13 +34,13 @@ func (mapper *SegmentIDMapper) GetID(kruxid string) uint64 {
 	if value, ok := mapper.cache[kruxid]; ok {
 		return value
 	}
-	query := mapper.frame.FilterFieldTopN(1, nil, "kruxid", kruxid)
+	query := mapper.field.FilterFieldTopN(1, nil, "kruxid", kruxid)
 	response, err := mapper.client.Query(query, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 	result := response.Result()
-	for _, ci := range result.CountItems {
+	for _, ci := range result.CountItems() {
 		mapper.cache[kruxid] = ci.ID
 		return ci.ID
 	}
@@ -51,7 +51,7 @@ func (mapper *SegmentIDMapper) GetID(kruxid string) uint64 {
 	mapper.cache[kruxid] = rowid
 	attrs := make(map[string]interface{})
 	attrs["kruxid"] = kruxid
-	aquery := mapper.frame.SetRowAttrs(rowid, attrs)
+	aquery := mapper.field.SetRowAttrs(rowid, attrs)
 	response, err = mapper.client.Query(aquery, nil)
 	if err != nil {
 		log.Fatal(err)
@@ -60,17 +60,17 @@ func (mapper *SegmentIDMapper) GetID(kruxid string) uint64 {
 	return rowid
 }
 
-func NewSegmentIDMapper(client *pilosa.Client, frame *pilosa.Frame) *SegmentIDMapper {
-	q := frame.TopN(10000000)
+func NewSegmentIDMapper(client *pilosa.Client, field *pilosa.Field) *SegmentIDMapper {
+	q := field.TopN(10000000)
 	r, err := client.Query(q, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
-	rows := len(r.Result().CountItems)
+	rows := len(r.Result().CountItems())
 
 	return &SegmentIDMapper{
 		client: client,
-		frame:  frame,
+		field:  field,
 		cache:  make(map[string]uint64),
 		rowcnt: uint64(rows),
 	}
@@ -87,35 +87,33 @@ func NewKruxBitIterator(reader io.Reader, mapper *SegmentIDMapper, viewerIndex u
 	}
 }
 
-func (c *KruxBitIterator) NextBit() (pilosa.Bit, error) {
+func (c *KruxBitIterator) NextRecord() (pilosa.Record, error) {
 	if ok := c.scanner.Scan(); ok {
 		c.line++
 		text := strings.TrimSpace(c.scanner.Text())
 		parts := strings.Split(text, "^-^")
 		if len(parts) < 2 {
-			return pilosa.Bit{}, fmt.Errorf("Invalid Krux line: %d", c.line)
+			return pilosa.Column{}, fmt.Errorf("Invalid Krux line: %d", c.line)
 		}
 
 		if parts[0] != c.viewerValue {
 			c.viewerIndex++
 			c.viewerValue = parts[0]
 		}
-		timestamp := 0
-		bit := pilosa.Bit{
-			RowID:     c.mapper.GetID(parts[1]),
-			ColumnID:  c.viewerIndex,
-			Timestamp: int64(timestamp),
+		bit := pilosa.Column{
+			RowID:    c.mapper.GetID(parts[1]),
+			ColumnID: c.viewerIndex,
 		}
 		return bit, nil
 	}
 	if err := c.scanner.Err(); err != nil {
 		log.Fatal("Error encountered reading file", err)
-		return pilosa.Bit{}, err
+		return pilosa.Column{}, err
 	}
-	return pilosa.Bit{}, io.EOF
+	return pilosa.Column{}, io.EOF
 }
 
-func eat(client *pilosa.Client, frame *pilosa.Frame, mapper *SegmentIDMapper, path string, viewerIndex uint64) uint64 {
+func eat(client *pilosa.Client, field *pilosa.Field, mapper *SegmentIDMapper, path string, viewerIndex uint64) uint64 {
 	log.Println("Attempting to ingest", path)
 	f, err := os.Open(path)
 	if err != nil {
@@ -130,7 +128,7 @@ func eat(client *pilosa.Client, frame *pilosa.Frame, mapper *SegmentIDMapper, pa
 	defer gr.Close()
 
 	iterator := NewKruxBitIterator(gr, mapper, viewerIndex)
-	err = client.ImportFrame(frame, iterator, 10000000)
+	err = client.ImportField(field, iterator, pilosa.OptImportBatchSize(1000000))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -140,7 +138,7 @@ func eat(client *pilosa.Client, frame *pilosa.Frame, mapper *SegmentIDMapper, pa
 
 func main() {
 	sindex := flag.String("index", "segmentation", "name of the pilosa index")
-	sframe := flag.String("frame", "membership", "name of the pilosa frame")
+	sfield := flag.String("field", "membership", "name of the pilosa field")
 	dir := flag.String("dir", ".", "directory location to search for gzip files")
 	suri := flag.String("uri", "localhost:10101", "uri location of the pilosa server")
 	flag.Parse()
@@ -151,26 +149,22 @@ func main() {
 		log.Fatal(err)
 	}
 
-	client := pilosa.NewClientWithURI(uri)
-	schema, err := client.Schema()
+	client, err := pilosa.NewClient(uri)
+	if err != nil {
+		log.Fatal(err)
+	}
+	schema := pilosa.NewSchema()
+	index := schema.Index(*sindex)
+	field := index.Field(*sfield)
+	err = client.SyncSchema(schema)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	index, err := schema.Index(*sindex, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	frame, err := index.Frame(*sframe, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	mapper := NewSegmentIDMapper(client, frame)
+	mapper := NewSegmentIDMapper(client, field)
 	viewerIndex := uint64(0)
 	for i := 0; i < len(files); i++ {
-		viewerIndex = eat(client, frame, mapper, files[i], viewerIndex)
+		viewerIndex = eat(client, field, mapper, files[i], viewerIndex)
 		log.Println("Ingested", viewerIndex, "viewers")
 	}
 }
